6b3044f22eabafb7d9008e156136702254b2c98f
nikkarus <nearofseven@gmail.com>
2018-02-20 15:41:43 -0500

    Beta 2.0
    Updated for export to Excel functionality

--- Click Recorder Beta.py
+++ Click Recorder Beta.py
@@ -2,12 +2,15 @@ import pyautogui as pag
 import pandas as pd
 import keyboard
 import time
+import openpyxl as xl
+import pandas as pd
+from pandas import ExcelWriter
+
 
 def sleep(var1):
 	time.sleep(var1)
 
-
-locList = pd.DataFrame(columns=[])
+locList = pd.DataFrame()
 endLoop = 2
 while endLoop == 2:
     try:
@@ -15,10 +18,18 @@ while endLoop == 2:
             locX, locY = pag.position()
             locList = locList.append([[locX, locY]], ignore_index=True)
             endLoop = 2
-            sleep(.5)
+            sleep(.2)
         elif keyboard.is_pressed("esc"):   #    press escape to end program
             endLoop = 1
             break
     except:
         pass
+locList.columns = ['X', 'Y']
 print(locList)
+
+recordName = input("Please input what you would like to call this sheet:")
+
+writer = ExcelWriter(str(recordName)+ ' ' + 'Click Recording' + '.xlsx')
+locList.to_excel(writer, 'Sheet1', index=False)
+writer.save()
+
